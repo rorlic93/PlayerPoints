9dd099dab11540e5104296d2cda7b08fe2402166
Rastimir Orlic <orlicrastimir@gmail.com>
2016-07-12 00:56:56 +0200

    Small fixes
    Input field values and selected player fix

--- players.php
+++ players.php
@@ -1,4 +1,19 @@
-<?php session_start();?>
+<?php 
+
+session_start();
+
+if(isset($_POST['calculate'])){
+	$_SESSION['opp'] = $_POST["Opp"];
+	$_SESSION['column1'] = $_POST["Column1"];
+	$_SESSION['mp'] = $_POST["MP"];
+	$_SESSION['fga'] = $_POST["FGA"];
+	$_SESSION['fg'] = $_POST['FG%'];
+	$_SESSION['ft'] = $_POST['FT%'];
+	$_SESSION['trb'] = $_POST['TRB'];
+	$_SESSION['ast'] = $_POST['AST'];
+	$_SESSION['tov'] = $_POST['TOV'];
+}	
+?>
 
 <!doctype html>
 <html class="no-js" lang="en" dir="ltr">
@@ -25,32 +40,32 @@
 			<div class="large-4 columns">
 				<div class="callout tijelo">
 					<form method="POST" action="php/get.php" style="position: static;">
-						<h6>Player: </h6>
+						<p >Player: </p >
 						<select name="Players" method="POST">
-							<option>...</option>
+							<option disabled="disabled" selected="selected"><?php echo @$_SESSION['player'] . " (was selected)"; 
 							<option value="Curry">Stephen Curry</option>
-							<option value="Durant">Keivn Durant</option>
+							<option value="Durant">Kevin Durant</option>
 							<option value="James">Lebron James</option>
 						</select>
-						<h6>Opponent - East or West(E or W): </h6>
-						<input type="text" name="Opp" value="W"/>
-						<h6>Win or lose situation (W or L)</h6>
-						<input type="text" name="Column1" value="W"/>
-						<h6>Minutes (mm:ss:ms): </h6>
-						<input type="text" name="MP" value="30:00:00"/>
-						<h6>Number of field goal attempts</h6>
-						<input type="text" name="FGA" value="25"/>
-						<h6>Field goal percentage: </h6>
-						<input type="text" name="FG%" value="0.6"/>
-						<h6>Free throw percentage: </h6>
-						<input type="text" name="FT%" value="0.7"/>
-						<h6>Total rebounds (offensive+deffensive): </h6>
-						<input type="text" name="TRB" value="6"/>
-						<h6>Assists: </h6>
-						<input type="text" name="AST" value="5"/>
-						<h6>Turnovers: </h6>
-						<input type="text" name="TOV" value="4"/>
-						<button type="submit" class="button alert">Calculate</button>
+						<p >Opponent - East or West(E or W): </p >
+						<input type="text" name="Opp" value="<?php echo @$_SESSION['opp']; ?>";/>
+						<p >Win or lose situation (W or L)</p >
+						<input type="text" name="Column1" value="<?php echo @$_SESSION['column1']; ?>" />
+						<p >Minutes (mm:ss:ms): </p >
+						<input type="text" name="MP" value="<?php echo @$_SESSION['mp']; ?>" />
+						<p >Number of field goal attempts</p >
+						<input type="text" name="FGA" value="<?php echo @$_SESSION['fga']; ?>" />
+						<p >Field goal percentage: </p >
+						<input type="text" name="FG%" value="<?php echo @$_SESSION['fg']; ?>" />
+						<p >Free throw percentage: </p >
+						<input type="text" name="FT%" value="<?php echo @$_SESSION['ft']; ?>" />
+						<p >Total rebounds (offensive+deffensive): </p >
+						<input type="text" name="TRB" value="<?php echo @$_SESSION['trb']; ?>" />
+						<p >Assists: </p >
+						<input type="text" name="AST" value="<?php echo @$_SESSION['ast']; ?>" />
+						<p >Turnovers: </p >
+						<input type="text" name="TOV" value="<?php echo @$_SESSION['tov']; ?>" />
+						<button type="submit" name="calculate" class="button alert">Calculate</button>
 					</form>
 				</div>
 			</div>
